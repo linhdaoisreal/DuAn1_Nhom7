052e662b0cde6d2cde05df4e3881c67e4a0433b2
Cuongneee <daocuong1011990@gmail.com>
2024-03-28 10:53:53 +0700

    update dữ liệu người dùng

--- public/taiKhoanvaTourcuatoi.php
+++ public/taiKhoanvaTourcuatoi.php
@@ -3,32 +3,29 @@
     <div class="p-4 md:px-14">
         <!-- Tiêu đề -->
         <div class="mr-6">
-            <h1 class="text-4xl font-semibold underline">Thông tin tài khoản và các Tuor đã đặt</h1>
+            <h1 class="text-4xl font-semibold underline text-orange-500">Thông tin tài khoản và các Tuor đã đặt</h1>
         </div>
         <!-- Nội dung chính -->
         <div class="grid grid-rows-1 gap-6 py-5 md:grid-cols-6 md:gap-10">
             <!-- Thông tin tài khoản -->
             <div class="flex-row md:col-span-2 leading-9">
-                <h1 class="font-semibold text-2xl py-3">Thông tin tài khoản</h1>
+                <h1 class="font-semibold text-2xl py-3 text-orange-400">Thông tin tài khoản</h1><br>
                 <!-- avarta -->
-                <div class="items-center flex justify-center">
-                    <img class="w-1/3 md:w-2/3 rounded-full border" src="./src/img/LogoDA1_final.png" alt="">
+                <div class="items-center flex justify-center mt-4">
+                <img style="width:200px;height:200px;border-radius:50%" src="./gallery/<?= $anh_dai_dien?>" alt=""><br>
                 </div>
-                <p class="font-medium">Tên: <span class="font-normal">Lorem isum dplor</span></p>
-                <p class="font-medium">Email: <span class="font-normal">liem@gamil.com</span></p>
-                <p class="font-medium">Mật khẩu: <span class="font-normal">xxxxxxxxxxx</span></p>
-                <p class="font-medium">Số điện thoại: <span class="font-normal">02310231023</span></p>
-                <p class="font-medium">Địa chỉ: <span class="font-normal">Lorem isum dplor member blam St'Dráppul</span>
+                <p class="font-medium mt-6 text-lg">Tên người dùng: <span class="font-normal"><?=$ho_ten ?></span></p>
+                <p class="font-medium text-lg">Email: <span class="font-normal"><?=$email ?></span></p>
+                <p class="font-medium text-lg">Số điện thoại: <span class="font-normal"><?=$so_dien_thoai?></span></p>
+                <p class="font-medium text-lg">Địa chỉ: <span class="font-normal"><?=$dia_chi?></span>
                 </p>
-                <p class="text-center cursor-pointer underline hover:text-orange-500 transition-all">
-                    <a href="">Cập nhật thông tin</a>
-                </p>
+                
 
             </div>
 
             <!-- Thông tin các tour đã đặt -->
             <div class="md:col-span-4">
-                <h1 class="font-semibold text-2xl py-3">Các tour đã đặt</h1>
+                <h1 class="font-semibold text-2xl py-3 text-orange-400">Các tour đã đặt</h1>
                 <div class="w-full my-3 border rounded-lg flex px-4 grid-cols-2 gap-4">
                     <div class="w-24 h-auto">
                         <img class="" src="./src/img/banner1.png" alt="">
